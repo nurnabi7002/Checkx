c2bce7b7b0d73eac7c24a87dc37f6d5e4fb54110
MD Nur Nabi Khan Rabib <nurnabi7002@gmail.com>
2025-06-10 16:56:04 +0600

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,25 @@
+console.log('Tackle Studio');
+ 
+// ABOUT SCROLL
+document.getElementById("about-btn").addEventListener("click", function() {
+            document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+        });
+
+
+// SERVICES SCROLL
+document.getElementById("services-btn").addEventListener("click", function() {
+            document.getElementById("services").scrollIntoView({ behavior: "smooth" });
+        });
+
+
+// CONTACT SCROLL
+document.getElementById("contact-btn").addEventListener("click", function() {
+            document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
+        });
+
+
+
+
+
+
+
